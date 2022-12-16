2c3f63c41ff6133eaa1055d0978365038558424b
Giuseppe De Ponte <giuseppedeponte@gmail.com>
2022-12-16 14:57:05 +0100

    feat: replace agir_dons with agir_cagnotte shortcode in lfi-agir-registration

--- lfi-agir-registration/lfi-agir-registration.php
+++ lfi-agir-registration/lfi-agir-registration.php
@@ -24,7 +24,7 @@ class Plugin
         add_action('elementor_pro/init', [$this, 'register_elementor_addons']);
         add_action('wp_enqueue_scripts', [$this, 'cookie_script']);
         add_shortcode('agir_signatures', [$this, 'signature_shortcode_handler']);
-        add_shortcode('agir_dons', [$this, 'dons_shortcode_handler']);
+        add_shortcode('agir_cagnotte', [$this, 'cagnotte_shortcode_handler']);
     }
 
     public function admin_init()
@@ -97,36 +97,36 @@ class Plugin
         return $count;
     }
 
-
-    function dons_shortcode_handler($atts, $content, $tag)
+    function cagnotte_shortcode_handler($atts, $content, $tag)
     {
-        $transient_key = 'agir_dons';
-
-        $count = get_transient($transient_key);
-
-        if ($count !== false) {
-            return $count;
+        if (!is_array($atts) || !isset($atts["slug"])) {
+            return "";
         }
 
+        $slug = $atts["slug"];
         $options = get_option('lfi_settings');
+        $cache_group = 'lfi-agir-registrations__cagnottes';
+        $url = $options['api_server'] . "/cagnottes/$slug/compteur/";
 
-        $url = $options['api_server'].'/api/2022/dons/';
+        $response = wp_cache_get($url, $cache_group);
 
-        $response = wp_remote_get($url, [
-            'headers' => [
-                'Content-type' => 'application/json',
-                'Authorization' => 'Basic '.base64_encode($options['api_id'].':'.$options['api_key']),
-                'X-Wordpress-Client' => $_SERVER['REMOTE_ADDR']
-            ]
-        ]);
+        if (!$response) {
+            $response = wp_remote_get($url, [
+                'headers' => [
+                    'Content-type' => 'application/json',
+                    'Authorization' => 'Basic ' . base64_encode($options['api_id'] . ':' . $options['api_key']),
+                    'X-Wordpress-Client' => $_SERVER['REMOTE_ADDR']
+                ]
+            ]);
 
-        if (is_wp_error($response) || $response['response']['code'] !== 200) {
-            return get_option("lfi_dons_stale");
+            if (is_wp_error($response) || $response['response']['code'] !== 200) {
+                return 0;
+            }
         }
+
+        wp_cache_set($url, $response, $cache_group);
 
         $count  = json_decode($response["body"])->totalAmount;
-        set_transient($transient_key, $count, 30);
-        update_option("lfi_dons_stale", $count, false);
 
         return $count;
     }
