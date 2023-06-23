03eea7704be105e7f85e3931c46f8e2f4eba259d
Giuseppe De Ponte <giuseppedeponte@gmail.com>
2023-06-23 11:30:41 +0200

    Ajout du plugin RSS Aggregator - Link to source au repo

---
+++ rss-aggregator-link-to-source/rss-aggregator-link-to-source.php
@@ -0,0 +1,21 @@
+<?php
+/*
+    Plugin Name: RSS Aggregator - Link to source
+    Description: Plugin ultrabasique pour lier les titres aux articles originaux
+    Author: Jill Royer
+    License: GPL-3.0
+*/
+
+function rss_aggregator_link_to_source($url, $post) {
+    if ($post->post_type == 'post') {
+        $permalink = get_post_meta($post->ID, "wprss_item_permalink", true);
+    }
+
+    if ($permalink !== '') {
+        return $permalink . '?" target="_blank';
+    }
+
+    return $url;
+}
+
+add_filter( 'post_link', 'rss_aggregator_link_to_source', 10, 3 );
