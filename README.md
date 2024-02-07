38285c5fafc218346c5e7bb100150d2aefe2f2f1
Salomé Cheysson <salome@cheysson.fr>
2024-02-07 16:04:24 +0100

    ajout d'un environnement de développement lando

---
+++ README.md
@@ -0,0 +1,3 @@
+# Environnement de développement
+
+Utilisez le script dev-setup.sh pour mettre en place l'environnement de développement.
---
+++ dev-setup.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+set -eu
+
+
+lando start
+
+lando wp core download \
+      --path=wordpress \
+      --locale=fr_FR
+
+
+lando wp config create \
+      --path=wordpress \
+      --dbname=wordpress \
+      --dbuser=wordpress \
+      --dbpass=wordpress \
+      --dbhost=database
+
+lando wp core install \
+      --path=wordpress \
+      --url=https://wordpress-plugins.lndo.site \
+      --title="Wordpress Plugins LFI" \
+      --admin_user=admin \
+      --admin_password=password \
+      --admin_email=admin@wordpress-plugins.lndo.site
+
+for plugin in lfi-settings lfi-agir-registration lfi-djan-stats; do
+    ln -s "../../../$plugin" "wordpress/wp-content/plugins/$plugin"
+done
