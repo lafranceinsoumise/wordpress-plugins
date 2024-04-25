cfbd75e614d95de3560e222548a15b02af33f5d1
Giuseppe De Ponte <giuseppedeponte@gmail.com>
2024-04-25 17:48:58 +0200

    envoi-senateurs: rnsp - campagne rouvreznosservicespublics_senateurs_2024-05

--- envoi-senateurs/lettre.php
+++ envoi-senateurs/lettre.php
@@ -64,6 +64,22 @@ function texte_lettre($senateur, $expediteur)
     ];
 }
 
+function campaign_id()
+{
+    return "rouvreznosservicespublics_senateurs_2024-05";
+}
+
+function texte_rs($senateur)
+{
+    return "Face aux effets délétères d'une dématérialisation tous azimuts, soutenez vous l'inscription et le vote au Sénat de la proposition de loi votée à l'Assemblée pour rouvrir les accueils physiques des services publics ⁇";
+}
+
+function tracking_attrs($piece, $senateur)
+{
+    $campaign_id = campaign_id();
+    $target = "$senateur[nom_complet] [$senateur[groupe]]";
+    return "data-track-content data-content-name='$campaign_id' data-content-piece='$piece' data-content-target='$target'";
+}
 
 function lien_twitter($senateur)
 {
@@ -72,14 +88,56 @@ function lien_twitter($senateur)
     }
 
     $url = "https://twitter.com/intent/tweet";
+    $text = texte_rs($senateur);
     $params = [
-        'text' => ".@$senateur[twitter] Guichets fermés, impossibilité d’obtenir un rendez-vous, obligation de passer par l’informatique… STOP ! Inscrivez à l’ordre du jour la proposition de loi transpartisane pour la réouverture des accueils physiques dans les services publics !",
+        'text' => "$senateur[civilite] @$senateur[twitter]. $text",
         'url' => 'https://RouvrezNosServicesPublics.fr'
     ];
 
     $url .= "?" . http_build_query($params);
+    $tracking_attrs = tracking_attrs("twitter", $senateur);
 
-    return "<a href='$url' target='__blank' rel='noopener noreferrer'>Interpellez $senateur[nom_complet] sur twitter</a>";
+    return "<div class='envoi' id='twitter'><a href='$url' target='_blank' rel='noopener noreferrer' $tracking_attrs>Interpellez $senateur[nom_complet] sur twitter</a></div>";
+}
+
+function lien_facebook($senateur)
+{
+    if (!$senateur["facebook"]) {
+        return "";
+    }
+
+    $params = [
+        'text' => texte_rs($senateur) . " https://RouvrezNosServicesPublics.fr"
+    ];
+    $url = "https://m.me/$senateur[facebook]";
+    $url .= "?" . http_build_query($params);
+    $tracking_attrs = tracking_attrs("facebook", $senateur);
+
+    return "<div class='envoi' id='facebook'><a href='$url' target='_blank' rel='noopener noreferrer' $tracking_attrs>Interpellez $senateur[nom_complet] via Messenger</a></div>";
+}
+
+function interpellation_rs($senateur)
+{
+    $texte_rs = texte_rs($senateur);
+    $lien_twitter = lien_twitter($senateur);
+    $lien_facebook = lien_facebook($senateur);
+
+    if (!$lien_twitter && !$lien_facebook) {
+        return "";
+    }
+
+    return <<<EOD
+      <h5>Interpellez $senateur[nom_complet] sur les réseaux sociaux</h5>
+
+      <p>
+        Voici le texte à utiliser pour interpeller <strong>$senateur[nom_complet]</strong> ($senateur[groupe]) sur les réseaux sociaux.
+      <p>
+
+      <blockquote>$texte_rs</blockquote>
+
+      $lien_twitter
+      $lien_facebook
+    EOD;
 }
 
 function generer_interpellation($senateur, $twitters, $facebooks, $expediteur)
@@ -92,6 +150,7 @@ function generer_interpellation($senateur, $twitters, $facebooks, $expediteur)
         return $result;
     }
 
+    $campaign_id = campaign_id();
     $texte = texte_lettre($senateur, $expediteur);
 
     $texte_lettre_html = implode(
@@ -113,17 +172,21 @@ function generer_interpellation($senateur, $twitters, $facebooks, $expediteur)
         "mailto:$senateur[email]?subject=$objet_lettre_email&body=$texte_lettre_email",
         ENT_QUOTES | ENT_HTML5,
     );
+    $tracking_attrs = tracking_attrs("email", $senateur);
 
-    $lien_twitter = lien_twitter($senateur);
+    $interpellation_rs = interpellation_rs($senateur);
 
     $result = <<<EOD
+
+      $interpellation_rs
+
+      <h5>Interpellez $senateur[nom_complet] par e-mail</h5>
+
       <p>
         Voici le texte généré à partir de vos informations, adressé pour l'exemple $senateur[recipient] <strong>$senateur[nom_complet]</strong> ($senateur[groupe]).
       <p>
 
-      <blockquote>
-        $texte_lettre_html
-      </blockquote>
+      <blockquote>$texte_lettre_html</blockquote>
 
       <p>
         Si vous envoyez le texte vous-même, il sera expédié $senateur[recipient] <strong>$senateur[nom_complet]</strong>.
@@ -141,12 +204,10 @@ function generer_interpellation($senateur, $twitters, $facebooks, $expediteur)
           <input type="hidden" name="prenom" value="$expediteur[prenom]">
           <input type="hidden" name="profession" value="$expediteur[profession]">
           <input type="hidden" name="civilite" value="$expediteur[civilite]">
-          <input type="hidden" name="campaign" value="rouvreznosservicespublics_senateurs_2024-01">
-          <a href="$lien_email">Je l'envoie moi-même</a>
+          <input type="hidden" name="campaign" value="$campaign_id">
+          <a target='_blank' rel='noopener noreferrer' href="$lien_email" $tracking_attrs>Je l'envoie moi-même</a>
           <button type="submit">Envoyez-le pour moi</button>
       </div>
-
-      <div class="envoi">$lien_twitter</div>
 
       <p id="success-message">
         Merci, votre email va être envoyé automatiquement. Vous en recevrez une copie sur l’adresse email que vous avez indiquée.
