c5c195434560e6c085b7a8b9b9267138b6a0030f
Giuseppe De Ponte <giuseppedeponte@gmail.com>
2024-04-29 15:15:46 +0200

    export-bus: add 2024-export-bus-meeting-bordeaux

--- export-bus/2024-export-bus-meeting-bordeaux.php
+++ export-bus/2024-export-bus-meeting-bordeaux.php
@@ -4,5 +4,5 @@ if (!defined('ABSPATH')) exit; // Exit if accessed directly
 
 require_once(dirname(__FILE__) . '/WC_Bus_Order_Exporter.php');
 
-// Bus meeting Marseille 23/05/2024
-(new WC_Bus_Order_Exporter(66246, "1RB6BeOLCoK9xieWITNVJeir_7WkPvWnN3VBJ-E8aGrY"))->export();
+// Bus meeting Bordeaux 18/05/2024
+(new WC_Bus_Order_Exporter(66917, "1gxaPSndHjvE1gJuljsKH9dxRkzFuM8vZdfQqwnishXk"))->export();
